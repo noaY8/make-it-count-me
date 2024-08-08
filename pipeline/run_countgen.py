0e49c2326cecc2afa7bbb7c19f4084b6c0d6c805
noaY8 <noayamin@mail.tau.ac.il>
2024-08-08 10:18:38 +0300

    Update run_countgen.py

--- pipeline/run_countgen.py
+++ pipeline/run_countgen.py
@@ -54,7 +54,7 @@ def init_sdxl_model(config):
     sdxl_pipe.to(device)
 
     #sdxl_pipe.gradient_checkpointing_enable()  # Enable gradient checkpointing, added by noa 08.08.24
-    #cpu_offload(sdxl_pipe)  # Offload model to CPU, added by noa 08.08.24
+    cpu_offload(sdxl_pipe)  # Offload model to CPU, added by noa 08.08.24
 
     sdxl_pipe.counting_config = config['counting_model']
 
